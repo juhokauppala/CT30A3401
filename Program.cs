923d8cd2c4335082959fcbbb2f1da2f1d8cf71c8
juhokauppala <juho.kauppala@gmail.com>
2020-01-15 16:30:37 +0200

    Initial commit. Code not working

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace distributed_chat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server.Start(args);
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+public class Server
+{
+    TcpListener server = null;
+
+    public Server() : this(12112)
+    {
+
+    }
+
+    public Server(Int32 port)
+    {
+
+    }
+}
